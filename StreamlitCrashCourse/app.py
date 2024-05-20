6010db66d8f173e970907d114f7924c67e44bfad
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2024-05-20 21:36:11 +0200

    Input Widgets

--- StreamlitCrashCourse/app.py
+++ StreamlitCrashCourse/app.py
@@ -24,4 +24,17 @@ st.error("This is error")
 st.warning("Warning")
 st.exception("Exception")
 
+
+# Input Widget
+first_name = st.text_input("Enter First Name:")
+password = st.text_input("Enter Password:", type="password")
+message = st.text_area("Message")
+data = st.date_input("Date")
+appointment_time = st.time_input("Appointment Time")
+age = st.number_input("Age", min_value=0, max_value=120)
+gender = st.radio("Gender", ["Male", "Female"])
+enable = st.toggle("Enable Picker")
+level = st.checkbox("Level")
+
+
 #sk-proj-qMHVSf8fGKKdPxxUw1kGT3BlbkFJAqw74CBx20CZ9RGNqOIV
